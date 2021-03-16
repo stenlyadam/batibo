3d9d3c815e59c660ed40ee828e26680a490378be
samuelrantung <samuelmrantung@gmail.com>
2021-03-16 14:51:17 +0800

    adding profile page

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,5 +3,23 @@ import IconArrowBack from './ic-arrow-back.svg';
 import IconMinus from './ic-minus.svg';
 import IconPlus from './ic-plus.svg';
 import IconCart from './ic-cart.svg';
+import IconDotsIcon from './ic-dots-option.svg';
+import IconCoupon from './ic-coupon.svg';
+import IconAddress from './ic-address.svg';
+import IconProtection from './ic-protection.svg';
+import IconHelp from './ic-help.svg';
+import IconArrowRight from './ic-arrow-right.svg';
 
-export {IconSearch, IconArrowBack, IconMinus, IconPlus, IconCart};
+export {
+  IconSearch,
+  IconArrowBack,
+  IconMinus,
+  IconPlus,
+  IconCart,
+  IconDotsIcon,
+  IconCoupon,
+  IconAddress,
+  IconProtection,
+  IconHelp,
+  IconArrowRight,
+};
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,5 +2,12 @@ import IMGLogoBatibo from './img-logo-batibo.png';
 import IMGOrderSuccess from './img-order-success.png';
 import IMGOnBoarding1 from './img-onboarding-1.png';
 import IMGWortel from './img-wortel.png';
+import IMGProfilePicture from './img-profile-pic.png';
 
-export {IMGLogoBatibo, IMGOrderSuccess, IMGOnBoarding1, IMGWortel};
+export {
+  IMGLogoBatibo,
+  IMGOrderSuccess,
+  IMGOnBoarding1,
+  IMGWortel,
+  IMGProfilePicture,
+};
